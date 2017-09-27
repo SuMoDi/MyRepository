450ea96dd9b2058b8b65f57e4eeffcb1058b706d
SuMoDi <32185618+SuMoDi@users.noreply.github.com>
2017-09-27 11:28:49 +0800

    Update easy_map.py

--- easy_map.py
+++ easy_map.py
@@ -1,5 +1,15 @@
+# easy_map
 def normalize(name):
     return name[0].upper() + name[1:].lower()
 
 L = ['adam', 'LISA', 'barT']
 print (list(map(normalize,L)))
+
+# easy_reduce
+from functools import reduce
+def ji(x,y):
+    return x*y
+
+def prod(L):
+    return reduce(ji,L)
+print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
