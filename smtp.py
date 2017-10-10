eec45e179f443ace634fd1ea967b53c7a5847d7a
SuMoDi <32185618+SuMoDi@users.noreply.github.com>
2017-10-10 11:09:21 +0800

    Create smtp.py

---
+++ smtp.py
@@ -0,0 +1,102 @@
+from email.mime.text import MIMEText
+msg = MIMEText('hello,send by Python...','plain','utf-8')
+
+# 输入Email地址和口令:
+from_addr = r'xiecx@asiainfo.com'
+password = r'abcd12345Q!'
+# 输入收件人地址:
+to_addr = r'412258371@qq.com'
+# 输入SMTP服务器地址:
+smtp_server = r'mail.asiainfo.com'
+
+import smtplib
+# 构造smtp服务中心
+server = smtplib.SMTP(smtp_server,25)
+# set_debuglevel(1)就可以打印出和SMTP服务器交互的所有信息
+server.set_debuglevel(1)
+# 邮箱登陆
+server.login(from_addr,password)
+# 发送邮件 发送方 接收方 邮件内容
+server.sendmail(from_addr,[to_addr],msg.as_string())
+server.quit()
+'''
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.utils import parseaddr,formataddr
+
+import smtplib
+
+def _format_addr(s):
+    name,addr = parseaddr(s)
+    return formataddr((Header(name,'utf-8').encode(),addr))
+
+# 输入Email地址和口令:
+from_addr = r'xiecx@asiainfo.com'
+password = r'abcd12345Q!'
+# 输入收件人地址:
+to_addr = r'412258371@qq.com'
+# 输入SMTP服务器地址:
+smtp_server = r'mail.asiainfo.com'
+
+# 发送文本邮件
+#msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+# 发送html邮件
+'''
+msg = MIMEText('<html><body><h1>Hello</h1>' +
+    '<p>send by <a href="http://www.python.org">Python</a>...</p>' +
+    '</body></html>', 'html', 'utf-8')
+'''
+
+# 带附件的邮件
+msg = MIMEMultipart()
+msg['From'] = _format_addr('Python爱好者 <%s>' % from_addr)
+msg['To'] = _format_addr('管理员 <%s>' % to_addr)
+msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
+'''
+# 邮件正文是MIMEText:
+msg.attach(MIMEText('send with file...', 'plain', 'utf-8'))
+
+# 添加附件就是加上一个MIMEBase，从本地读取一个图片:
+with open(r'E:\Python\File_test\test.png', 'rb') as f:
+    # 设置附件的MIME和文件名，这里是png类型:
+    mime = MIMEBase('image', 'png', filename='test.png')
+    # 加上必要的头信息:
+    mime.add_header('Content-Disposition', 'attachment', filename='test.png')
+    mime.add_header('Content-ID', '<0>')
+    mime.add_header('X-Attachment-Id', '0')
+    # 把附件的内容读进来:
+    mime.set_payload(f.read())
+    # 用Base64编码:
+    encoders.encode_base64(mime)
+    # 添加到MIMEMultipart:
+    msg.attach(mime)
+'''
+# 邮件内容中包含图片
+# 邮件正文是MIMEText:
+msg.attach(MIMEText('<html><body><h1>Hello</h1>' +
+    '<p><img src="cid:0"></p>' +
+    '</body></html>', 'html', 'utf-8'))
+
+# 添加附件就是加上一个MIMEBase，从本地读取一个图片:
+with open(r'E:\Python\File_test\test.png', 'rb') as f:
+    # 设置附件的MIME和文件名，这里是png类型:
+    mime = MIMEBase('image', 'png', filename='test.png')
+    # 加上必要的头信息:
+    mime.add_header('Content-Disposition', 'attachment', filename='test.png')
+    mime.add_header('Content-ID', '<0>')
+    mime.add_header('X-Attachment-Id', '0')
+    # 把附件的内容读进来:
+    mime.set_payload(f.read())
+    # 用Base64编码:
+    encoders.encode_base64(mime)
+    # 添加到MIMEMultipart:
+    msg.attach(mime)
+
+server = smtplib.SMTP(smtp_server, 25)
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
